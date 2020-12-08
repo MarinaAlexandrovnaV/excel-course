c930670be8f85e26590af2cead110682a99d0292
Marinapineapple <34007159+Marinapineapple@users.noreply.github.com>
2020-12-08 10:34:53 +0300

    Framework start (#4)
    * Finish base framework functional
    
    Co-authored-by: Marina <marina.vasileva@traveloffice.pro>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import './module'
+// import './module'
+import { Excel } from '@/components/excel/Excel'
+import { Formula } from '@/components/formula/Formula'
+import { Header } from '@/components/header/Header'
+import { Table } from '@/components/Table/Table'
+import { Toolbar } from '@/components/toolbar/Toolbar'
 import './scss/index.scss'
+// console.log('Working!')
 
+const excel = new Excel('#app', {
+    components: [Header, Toolbar, Formula, Table]
+})
 
-console.log('Working!')
+excel.render()
